30cef246a4e1233f2114ee2ec6420022e8557342
Adithya <adithya_murali@berkeley.edu>
2015-07-06 09:34:33 -0700

    animesh's changes to tsne.py

--- examples/c3d_feature_extraction/script/tsne.py
+++ examples/c3d_feature_extraction/script/tsne.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 from sklearn import (manifold, datasets, decomposition, ensemble, lda, random_projection, preprocessing)
+from scipy import linalg
+
 PATH_TO_DATA = 'data/'
 
 
@@ -21,7 +23,34 @@ for i in range(2, 100):
 print("Computing t-SNE embedding")
 tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
 t0 = time()
-X_scaled = preprocessing.scale(X)
 X_tsne = tsne.fit_transform(X)
 
+#----------------------------------------------------------------------
+# Pre-processing
+X_scaled = preprocessing.scale(X) #zero mean, unit variance
+X_tsne_scaled = tsne.fit_transform(X_scaled)
+
+#normalize the data (scaling individual samples to have unit norm)
+X_normalized = preprocessing.normalize(X, norm='l2')
+X_tsne_norm = tsne.fit_transform(X_normalized)
+
+#whiten the data 
+scaler = preprocessing.StandardScaler( with_std=False)
+X_centered = scaler.fit(X)
 IPython.embed()
+U, s, Vh = linalg.svd(X_centered)
+eps = 1
+invS = np.diag (np.reciprocal(np.sqrt(s+eps)))
+
+#PCA_whiten
+X_pca = np.dot(invS, np.dot(U.T, X_centered))
+X_tsne_pca = tsne.fit_transform(X_pca)
+
+#whiten the data (ZCA)
+X_zca = np.dot(U, X_pca)
+X_tsne_zca = tsne.fit_transform(X_zca)
+
+
+IPython.embed()
+
+# for x in plots
